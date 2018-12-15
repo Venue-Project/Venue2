f124d3a3abb1902c3448fe100dbaab66cd6aa570
hypeboard <37452765+hypeboard@users.noreply.github.com>
2018-12-15 12:28:12 -0600

    ports

--- src/p2p/net_node.h
+++ src/p2p/net_node.h
@@ -127,14 +127,14 @@ namespace nodetool
     virtual std::map<std::string, time_t> get_blocked_hosts() { CRITICAL_REGION_LOCAL(m_blocked_hosts_lock); return m_blocked_hosts; }
   private:
     const std::vector<std::string> m_seed_nodes_list =
-    { "euseed1.x-cash.org:18280"
-    ,  "euseed2.x-cash.org:18280"
-    ,  "euseed3.x-cash.org:18280"
-    ,  "usseed1.x-cash.org:18280"
-    ,  "usseed2.x-cash.org:18280"
-    ,  "usseed3.x-cash.org:18280"
-    ,  "asiaseed1.x-cash.org:18280"
-    ,  "asiaseed2.x-cash.org:18280"
+    { "euseed1.x-cash.org:14219"
+    ,  "euseed2.x-cash.org:14219"
+    ,  "euseed3.x-cash.org:14219"
+    ,  "usseed1.x-cash.org:14219"
+    ,  "usseed2.x-cash.org:14219"
+    ,  "usseed3.x-cash.org:14219"
+    ,  "asiaseed1.x-cash.org:14219"
+    ,  "asiaseed2.x-cash.org:14219"
     };
     bool islimitup=false;
     bool islimitdown=false;
