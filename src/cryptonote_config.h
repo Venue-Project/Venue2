1b027ecee0042c637f34de8ffa5057b418897744
AR Crypto <37816162+ALLRiPPED@users.noreply.github.com>
2018-12-15 17:36:47 -0600

    new Genesis & port changes to testnet and stagenet

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -204,9 +204,9 @@ namespace config
     uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 0x22c23a8;
     uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 0x2c23a8;
     uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 63;
-    uint16_t const P2P_DEFAULT_PORT = 28280;
-    uint16_t const RPC_DEFAULT_PORT = 28281;
-    uint16_t const ZMQ_RPC_DEFAULT_PORT = 28282;
+    uint16_t const P2P_DEFAULT_PORT = 24220;
+    uint16_t const RPC_DEFAULT_PORT = 24221;
+    uint16_t const ZMQ_RPC_DEFAULT_PORT = 24222;
     boost::uuids::uuid const NETWORK_ID = { {
         0x10 ,0x10, 0x41, 0x53 , 0x48, 0x62 , 0x41, 0x65, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x11
       } };
@@ -219,9 +219,9 @@ namespace config
     uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 24;
     uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 25;
     uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 36;
-    uint16_t const P2P_DEFAULT_PORT = 38080;
-    uint16_t const RPC_DEFAULT_PORT = 38081;
-    uint16_t const ZMQ_RPC_DEFAULT_PORT = 38082;
+    uint16_t const P2P_DEFAULT_PORT = 34220;
+    uint16_t const RPC_DEFAULT_PORT = 34221;
+    uint16_t const ZMQ_RPC_DEFAULT_PORT = 34222;
     boost::uuids::uuid const NETWORK_ID = { {
         0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x12
       } }; // Bender's daydream
